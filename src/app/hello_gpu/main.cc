a858eb69eb8afe3de58d71f93cde43d719f46720
Alexander Senier <alex@senier.net>
2016-12-20 19:43:47 +0100

    WIP: Ring buffer mode of scheduling experiments
    When writing RING_BUFFER_* registers on SKL, the value read back
    always is 0. Have they removed direct ring buffer access? Looks
    like we'll have to implement execlist-based submission just now.

--- src/app/hello_gpu/main.cc
+++ src/app/hello_gpu/main.cc
@@ -12,10 +12,29 @@ using namespace Genode;
 
 Genode::size_t Component::stack_size() { return 64*1024; }
 static Timer::Connection timer;
+static Platform::Connection pci;
 
-struct GMA : public Mmio
+/**
+ * Allocate DMA memory from the PCI driver
+ */
+static Genode::Ram_dataspace_capability alloc_dma_memory(Genode::Env &env, Genode::size_t size)
 {
-	GMA(addr_t const base) : Mmio(base) { }
+    size_t donate = size;
+
+    return Genode::retry<Platform::Session::Out_of_metadata>(
+        [&] () { return pci.alloc_dma_buffer(size); },
+        [&] () {
+            char quota[32];
+            Genode::snprintf(quota, sizeof(quota), "ram_quota=%zd",
+                             donate);
+            env.parent().upgrade(pci.cap(), quota);
+            donate = donate * 2 > size ? 4096 : donate * 2;
+        });
+}
+
+struct IGD : public Mmio
+{
+	uint64_t *_gtt;
 
 	struct FAULT_REG : Register<0x4094, 32>
 	{
@@ -44,9 +63,106 @@ struct GMA : public Mmio
 
 	struct TIMESTAMP_CTR : Register<0x44070, 32> { };
 
-	void check()
+	// FIXME: Use one structure for rings
+
+	struct RING_BUFFER_TAIL_RCSUNIT : Register<0x02030, 32>
 	{
-		Genode::log ("FAULT_REG.Valid: ", read<FAULT_REG::Valid_Bit>());
+		struct Tail_Offset : Bitfield< 3, 18> { };
+	};
+
+	struct RING_BUFFER_HEAD_RCSUNIT : Register<0x02034, 32>
+	{
+		struct Wrap_Count  : Bitfield<21, 11> { };
+		struct Head_Offset : Bitfield< 2, 19> { };
+	};
+
+	struct RING_BUFFER_START_RCSUNIT : Register<0x02038, 32>
+	{
+		struct Starting_Address : Bitfield<12, 20> { };
+	};
+
+	struct RING_BUFFER_CTL_RCSUNIT : Register<0x0203C, 32>
+	{
+		struct Buffer_Length                 : Bitfield<12, 9> { };
+		struct RBWait                        : Bitfield<11, 1> { };
+		struct SemaphoreWait                 : Bitfield<10, 1> { };
+		struct Automatic_Report_Head_Pointer : Bitfield< 1, 2>
+		{
+			enum {
+				MI_AUTOREPORT_OFF    = 0,
+				MI_AUTOREPORT_64KB   = 1,
+				MI_AUTOREPORT_4KB    = 2,
+				MI_AUTO_REPORT_128KB = 3
+			};
+		};
+		struct Ring_Buffer_Enable            : Bitfield< 0, 1> { };
+	};
+
+	struct ACTHD_RCSUNIT : Register<0x2074, 32>
+	{
+		struct Head_Pointer : Bitfield<2, 30> { };
+	};
+
+	struct GFX_MODE : Register<0x0229C, 32>
+	{
+		struct Execlist_Enable           : Bitfield<15, 1> { };
+		struct PPGTT_Enable              : Bitfield< 9, 1> { };
+		struct Virtual_Addressing_Enable : Bitfield< 7, 1> { };
+		struct Privilege_Check_Disable   : Bitfield< 0, 1> { };
+	};
+
+	IGD (Genode::Env &env, addr_t const base) : Mmio(base)
+	{
+		// Allocate one page of DMA memory
+		static const unsigned ring_size = 4096;
+		Ram_dataspace_capability ring_ds = alloc_dma_memory (env, ring_size);
+		if (!ring_ds.valid())
+			throw -1;
+
+		uint8_t *ring_addr = env.rm().attach(ring_ds);
+		Genode::log ("Attached DMA buffer to ", Hex((unsigned long long)ring_addr));
+
+		_gtt = (uint64_t *)(base + 0x800000);
+		for (int i = 0; i < 10; i++)
+		{
+			Genode::log ("GTT[", i, "]: ", Hex((unsigned long long)_gtt[i]));
+		}
+
+		// Map start of GTT to physical page
+		uint64_t pte = (Genode::Dataspace_client (ring_ds).phys_addr() | 1);
+		_gtt[0] = pte;
+
+		typedef RING_BUFFER_CTL_RCSUNIT::Automatic_Report_Head_Pointer ARHP;
+
+		Genode::log ("TSC: ", Hex (read<TIMESTAMP_CTR>()));
+
+		Genode::log ("GFX_MODE");
+		Genode::log ("   Execlist_Enable:           ", Hex (read<GFX_MODE::Execlist_Enable>()));
+		Genode::log ("   PPGTT_Enable:              ", Hex (read<GFX_MODE::PPGTT_Enable>()));
+		Genode::log ("   Virtual_Addressing_Enable: ", Hex (read<GFX_MODE::Virtual_Addressing_Enable>()));
+		Genode::log ("   Privilege_Check_Disable:   ", Hex (read<GFX_MODE::Privilege_Check_Disable>()));
+
+		// Set ring buffer to start of GTT
+		Genode::log ("Setting up ring buffer (1)");
+		Genode::log ("   Starting_Address:   ", Hex (read<RING_BUFFER_START_RCSUNIT::Starting_Address>()));
+		Genode::log ("   Ring_Buffer_Enable: ", Hex (read<RING_BUFFER_CTL_RCSUNIT::Ring_Buffer_Enable>()));
+		Genode::log ("   Buffer_Length:      ", Hex (read<RING_BUFFER_CTL_RCSUNIT::Buffer_Length>()));
+
+		write<RING_BUFFER_START_RCSUNIT::Starting_Address>(4);
+		write<RING_BUFFER_HEAD_RCSUNIT::Head_Offset>(44);
+		write<RING_BUFFER_TAIL_RCSUNIT::Tail_Offset>(44);
+		write<RING_BUFFER_CTL_RCSUNIT::Buffer_Length> (0);
+		write<RING_BUFFER_CTL_RCSUNIT::Automatic_Report_Head_Pointer> (ARHP::MI_AUTOREPORT_OFF);
+		write<RING_BUFFER_CTL_RCSUNIT::Ring_Buffer_Enable>(1);
+		
+		Genode::log ("Setting up ring buffer (2)");
+		Genode::log ("   Starting_Address:   ", Hex (read<RING_BUFFER_START_RCSUNIT::Starting_Address>()));
+		Genode::log ("   Ring_Buffer_Enable: ", Hex (read<RING_BUFFER_CTL_RCSUNIT::Ring_Buffer_Enable>()));
+		Genode::log ("   Buffer_Length:      ", Hex (read<RING_BUFFER_CTL_RCSUNIT::Buffer_Length>()));
+
+		unsigned head_pointer = read<ACTHD_RCSUNIT::Head_Pointer>();
+		Genode::log ("ACTHD: ", Hex (head_pointer, Hex::OMIT_PREFIX));
+		Genode::log ("TSC: ", Hex (read<TIMESTAMP_CTR>()));
 	}
 };
 
@@ -82,8 +198,6 @@ static void print_device_info (Platform::Device_capability device_cap)
     }
 }
 
-static Platform::Connection pci;
-
 static Platform::Device_capability find_gpu_device (Genode::Env &env)
 {
 	unsigned char bus = 0, dev = 0, fun = 0;
@@ -109,24 +223,6 @@ static Platform::Device_capability find_gpu_device (Genode::Env &env)
 	return dev_cap;
 }
 
-/**
- * Allocate DMA memory from the PCI driver
- */
-static Genode::Ram_dataspace_capability alloc_dma_memory(Genode::Env &env, Genode::size_t size)
-{
-    size_t donate = size;
-
-    return Genode::retry<Platform::Session::Out_of_metadata>(
-        [&] () { return pci.alloc_dma_buffer(size); },
-        [&] () {
-            char quota[32];
-            Genode::snprintf(quota, sizeof(quota), "ram_quota=%zd",
-                             donate);
-            env.parent().upgrade(pci.cap(), quota);
-            donate = donate * 2 > size ? 4096 : donate * 2;
-        });
-}
-
 void config_write(Genode::Env &env, Platform::Device_client *device,
 				  uint8_t op, uint16_t cmd,
                   Platform::Device::Access_size width)
@@ -145,6 +241,10 @@ void config_write(Genode::Env &env, Platform::Device_client *device,
 
 void Component::construct(Genode::Env &env)
 {
+	enum {
+		PCI_CMD_REG = 4,
+	};
+
 	Io_mem_session_capability io_mem;
 
 	// Open connection to PCI service
@@ -153,103 +253,43 @@ void Component::construct(Genode::Env &env)
 	Genode::log ("Hello GPU!");
 
 	gpu_cap = find_gpu_device (env);
-	if (gpu_cap.valid())
-	{
-		Genode::log ("Found GPU device");
-		print_device_info (gpu_cap);
-    	Platform::Device_client device(gpu_cap);
+	if (!gpu_cap.valid())
+		throw -1;
 
-		enum {
-			PCI_CMD_REG = 4,
-		};
+	Genode::log ("Found GPU device");
+	print_device_info (gpu_cap);
+    Platform::Device_client device(gpu_cap);
 
-		// Enable bus master
-		uint16_t cmd = device.config_read(PCI_CMD_REG, Platform::Device::ACCESS_16BIT);
-		cmd |= 0x4;
-		config_write(env, &device, PCI_CMD_REG, cmd, Platform::Device::ACCESS_16BIT);
+	// Enable bus master
+	uint16_t cmd = device.config_read(PCI_CMD_REG, Platform::Device::ACCESS_16BIT);
+	cmd |= 0x4;
+	config_write(env, &device, PCI_CMD_REG, cmd, Platform::Device::ACCESS_16BIT);
 
-		// Print PCI config values
-		uint64_t GTTMMADR_low  = device.config_read(0x10, Platform::Device::ACCESS_32BIT);
-		uint64_t GTTMMADR_high = device.config_read(0x14, Platform::Device::ACCESS_32BIT);
-		uint64_t GTTMMADR = (GTTMMADR_high << 32) | GTTMMADR_low;
-		Genode::log ("GTTMMADR: ", Hex(GTTMMADR));
+	// Map BAR0
+	Platform::Device::Resource const bar0 = device.resource(0);
+	Io_mem_connection bar0_mem (bar0.base(), bar0.size());
+	bar0_mem.on_destruction(Genode::Io_mem_connection::KEEP_OPEN);
+	Io_mem_dataspace_capability bar0_ds = bar0_mem.dataspace();
 
-		uint16_t GGC_0_0_0_PCI = device.config_read(0x50, Platform::Device::ACCESS_16BIT);
-		Genode::log ("GGC_0_0_PCI: ", Hex(GGC_0_0_0_PCI));
+	if (!bar0_ds.valid())
+		throw -1;	
 
-		uint32_t BDSM_0_0_0_PCI = device.config_read(0xb0, Platform::Device::ACCESS_32BIT);
-		Genode::log ("BDSM_0_0_PCI: ", Hex(BDSM_0_0_0_PCI));
+#if 0
+	// Map BAR2
+	Platform::Device::Resource const bar2 = device.resource(2);
 
-		uint32_t BGSM_0_0_0_PCI = device.config_read(0xb4, Platform::Device::ACCESS_32BIT);
-		Genode::log ("BGSM_0_0_PCI: ", Hex(BGSM_0_0_0_PCI));
+	Io_mem_connection bar2_mem (bar2.base(), bar2.size());
+	bar2_mem.on_destruction(Genode::Io_mem_connection::KEEP_OPEN);
+	Io_mem_dataspace_capability bar2_ds = bar2_mem.dataspace();
+	if (!bar2_ds.valid())
+		throw -1;	
 
-		// Map BAR0
-		Platform::Device::Resource const bar0 = device.resource(0);
+	uint8_t *aperture_addr = env.rm().attach(bar2_ds, bar2.size());
+#endif
 
-		Io_mem_connection bar0_mem (bar0.base(), bar0.size());
-		bar0_mem.on_destruction(Genode::Io_mem_connection::KEEP_OPEN);
-		Io_mem_dataspace_capability bar0_ds = bar0_mem.dataspace();
-		if (!bar0_ds.valid())
-			throw -1;	
-
-		uint8_t *gma_addr = env.rm().attach(bar0_ds, bar0.size());
-		Genode::log ("Attached BAR0 MMIO to ", Hex((unsigned long long)gma_addr), " size ", bar0.size());
-
-		// Map BAR2
-		Platform::Device::Resource const bar2 = device.resource(2);
-
-		Io_mem_connection bar2_mem (bar2.base(), bar2.size());
-		bar2_mem.on_destruction(Genode::Io_mem_connection::KEEP_OPEN);
-		Io_mem_dataspace_capability bar2_ds = bar2_mem.dataspace();
-		if (!bar2_ds.valid())
-			throw -1;	
-
-		uint8_t *aperture_addr = env.rm().attach(bar2_ds, bar2.size());
-		Genode::log ("Attached BAR2 (aperture) to ", Hex((unsigned long long)aperture_addr), " size ", bar2.size());
-
-		// Do something...
-		GMA gma ((addr_t) gma_addr);
-		gma.check();
-
-		// Allocate one page of DMA memory
-		static const unsigned dma_size = 4096;
-		Ram_dataspace_capability dma_ds;
-
-		dma_ds = alloc_dma_memory (env, dma_size);
-		if (!dma_ds.valid())
-			throw -1;
-
-		uint8_t *dma_addr = env.rm().attach(dma_ds);
-		Genode::log ("Attached DMA buffer to ", Hex((unsigned long long)dma_addr));
-
-		// Dump start of GTT
-		uint64_t *gtt = (uint64_t *)(gma_addr + 0x800000);
-		for (int i = 0; i < 10; i++)
-		{
-			Genode::log ("GTT[", i, "]: ", Hex((unsigned long long)gtt[i]));
-		}
-
-		// Map start of GTT to physical page
-		uint64_t pte = (Genode::Dataspace_client (dma_ds).phys_addr() | 1);
-		Genode::log ("Writing PTE to GTT[0]: ", Hex((unsigned long long)pte));
-		gtt[0] = pte;
-		Genode::log ("Done");
-
-		// We now should be able to write to dma_addr and read the same values back via aperture.
-		dma_addr[0] = 'd';
-		dma_addr[1] = 'e';
-		dma_addr[2] = 'a';
-		dma_addr[3] = 'd';
-		dma_addr[4] = 'b';
-		dma_addr[5] = 'e';
-		dma_addr[6] = 'e';
-		dma_addr[7] = 'f';
-		dma_addr[8] = '\0';
-
-		Genode::log ("Aperture value: ", Genode::Cstring((char const *)aperture_addr));
-
-		pci.release_device (gpu_cap);
-	}
-
+	// Dump start of GTT
+	uint8_t *igd_addr = env.rm().attach(bar0_ds, bar0.size());
+	IGD igd (env, (addr_t) igd_addr);
+	pci.release_device (gpu_cap);
 	Genode::log ("Done");
 }
